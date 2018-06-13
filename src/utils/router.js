7569b86e08982436da9b78e7d49438070f91c809
v_yguiliu <v_yguiliu@tencent.com>
2018-06-13 20:15:32 +0800

    feat: 实现定义路由对象

--- src/utils/router.js
+++ src/utils/router.js
@@ -1,20 +1,74 @@
 import React from 'react'
-import {Route} from 'react-router-dom'
+import {BrowserRouter, Route, Switch} from 'react-router-dom'
+
+function makeRoute (route) {
+  return (
+    <Route
+      exact={route.exact}
+      path={route.path}
+      key={route.name ? route.name : route.path}
+      component={route.component}
+      />
+  )
+}
+
+/**
+* @param {string} path 路径
+* @return {string} 返回连接路径
+**/
+function reslove () {
+  let paths = []
+  for (let i in arguments) {
+    paths.push(arguments[i].replace(/^\s*|\s*$/g, '').replace(/^\//, ''))
+  }
+  return paths.join('/')
+}
 
 export default class Router {
   constructor (router) {
-    let isExact = typeof router.exact !== 'undefined' ? router.exact : true
-    return function () {
-      return router.routes.map((router, index) => {
-        return (
-          <Route
-            exact={isExact}
-            path={router.path}
-            key={router.path + '' + index}
-            component={router.component}
-            />
-        )
-      })
+    let routes = router.routes
+    this.routerMap = {
+      'default': []
     }
+
+    for (let i in routes) {
+      if (Array.isArray(routes[i].children)) {
+        this.routerMap['default'].unshift(makeRoute({...routes[i], exact: false}))
+        this.routerMap[routes[i].name] = []
+        for (let j in routes[i].children) {
+          let child = routes[i].children[j]
+          let path = ''
+          if (child.name !== 404) {
+            path = reslove('/', routes[i].path, routes[i].children[j].path)
+          }
+          this.routerMap[routes[i].name].push(makeRoute({
+            ...routes[i].children[j],
+            exact: true,
+            path: path
+          }))
+        }
+      } else {
+        this.routerMap['default'].push(makeRoute({...routes[i], exact: true}))
+      }
+    }
+  }
+
+  view (name = 'default') {
+    if (!this.routerMap[name]) throw new Error('子路由不存在')
+    let that = this
+    if (name === 'default') {
+      return (
+        <BrowserRouter >
+          <Switch>
+            {that.routerMap[name]}
+          </Switch>
+        </BrowserRouter>
+      )
+    }
+    return (
+      <Switch>
+        {that.routerMap[name]}
+      </Switch>
+    )
   }
 }
